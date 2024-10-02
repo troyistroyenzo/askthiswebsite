99c3965db11cd5e998198c7cc6087979b1e6c3ef
Troy <yortozne@gmail.com>
2024-10-02 12:37:04 +0800

    added functionality

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,12 +1,40 @@
+import { ChatWrapper } from "@/components/ChatWrapper";
+import { ragChat } from "@/lib/rag-chat"
+import { redis } from "@/lib/redis";
+
+
 interface PageProps {
     params:{ 
         url: string | string[] | undefined
     }
 }
 
-const Page = ({params}: PageProps) => {
-    console.log(params)
-    return <p>Hi</p>
+function reconstructUrl({ url }: { url: string[] }) {
+  const decodedComponents = url.map((component) => decodeURIComponent(component));
+
+  return decodedComponents.join("//");
+}
+
+const Page = async ({params}: PageProps) => {
+    const reconstructedUrl = reconstructUrl({ url: params.url as string[] });
+   
+    const isAlreadyIndexed = await redis.sismember("indexed-urls", reconstructedUrl)
+
+    const sessionId = "mock-session"
+
+    console.log('isAlreadyIndexed', isAlreadyIndexed)
+    if (!isAlreadyIndexed) {
+        await ragChat.context.add({
+          type: "html",
+          source: reconstructedUrl,
+          config: { chunkOverlap: 50, chunkSize: 200 },
+        });
+    
+        await redis.sadd("indexed-urls", reconstructedUrl);
+      }
+
+
+    return <ChatWrapper sessionId={sessionId}/>
 }
 
 export default Page;
